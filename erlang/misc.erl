ddc61e61509e5eb36fe8d9f0757e5ce9810b5318
lucashungaro <lucashungaro@gmail.com>
2009-02-26 15:44:04 -0300

    Moving through chapter 8

--- erlang/misc.erl
+++ erlang/misc.erl
@@ -258,3 +258,40 @@ receive
 end 
 % Receives a message that has been sent to a process.
 
+% Receive with timeout:
+receive 
+    Pattern1 [when Guard1] -> 
+                     Expressions1; 
+    Pattern2 [when Guard2] -> 
+                     Expressions2; 
+    ... 
+after Time -> 
+    Expressions 
+end
+
+% Sample usages
+sleep(T) -> 
+    receive 
+    after T -> 
+            true 
+    end.
+
+flush_buffer() -> 
+    receive 
+        _Any -> 
+            flush_buffer() 
+    after 0 -> 
+            true 
+    end. 
+
+priority_receive() -> 
+    receive 
+        {alarm, X} -> 
+            {alarm, X} 
+    after 0 -> 
+            receive 
+                Any -> 
+                    Any 
+            end 
+    end.
+
